1d93fa18819ee1b6a64e249b7cedb26ce1903032
Hector Baldaguez <Hbaldaguez@fullsail.edu>
2014-03-13 23:16:37 -0400

    Added the boolean and number funcion but not finished.

--- BaldaguezRodriguez_Hector_Project2.js
+++ BaldaguezRodriguez_Hector_Project2.js
@@ -2,47 +2,83 @@
 // SDI 1403
 // 03/13/2014
 // Project 2
-//
+
 
 
 // My Variables
 var title = "BladeBow Hunter";
 var myDecision = "yes"
-var hitPoints;
 var huntBord = "Hunting Board";
-var scraplerHP = 2700;
-var gunnerHP = 3000;
 var userName;
 
 var say = function (message) {console.log(message);};
-
 // My Prompt and Proceduer 
+alert("Welcome to " + title)
+userName = prompt("What is your Hunter name?");
 
-userName = prompt("What will be you'er Hunter name","Hunter Name");
-
-var user = function () {
+var userN = function (hunter) {
+    
     say("Your Hunter Name is " + userName);
     
-}
-user()
+};
+userN(name)
 
 // Confrimation
 
-myDecision = confirm("Want to play " + title + " ?");
 
-var decision = function () {
+myDecision = confirm("Choose a Mission from the " + huntBord + ". Ok for Scrapler or Cancel for Gunner");
+
+var decision = function (mission) {
     
     if (myDecision === true) {
-        //Decision to play or not
+        //Mission to choose.
         say("Let's go hunting.");
     } else {
-        say("To bad. I guess you'll be the hunted then.");
+        say("Let's take him down.");
     };
     return;
 };
+decision();
 
-decision()
+// My battle section
 
+var attaking = function (hitPoints) {
+    
+    var attack = true;
+    
+    while (hitPoints > -330) {
+        
+        say("Slash him to death");
+        say("He's going down only " + hitPoints + " HP left");
+        attack = true
+        
+        attack = confirm("Time to hunt. OK to attack and Cancel to escape."); 
+        
+        if (attack === true) {
+            hitPoints = hitPoints -370;
+        } else {
+            say("To bad.");
+            attack = false 
+            
+        }   
+        
+    }     
+    return hitPoints;
+};             
+var hp = attaking(3000);
+say("The monster has " + hp + " life left");
 
+// This part is for the cunsumption of experience points.
 
-
+var exp = function () { 
+    var levelInc = 150;
+    var expPoints = 1200;
+    while (expPoints >= levelInc){
+        say("Incrising the level of the weapon by " + levelInc);
+        
+    };    
+        for (levelInc = 150; expPoints <= 1200; levelInc++){
+            
+        };
+    
+};  
